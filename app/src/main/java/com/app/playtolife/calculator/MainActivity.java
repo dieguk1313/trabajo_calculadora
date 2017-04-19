c0a25f0c11767d31ed0f274629c8ac4910ccfe68
David Montoya <jdmontoyam@gmail.com>
2017-04-18 19:09:01 -0500

    Se crea el diseño de la actividad calculator

--- app/src/main/java/com/app/playtolife/calculator/MainActivity.java
+++ app/src/main/java/com/app/playtolife/calculator/MainActivity.java
@@ -1,13 +1,33 @@
 package com.app.playtolife.calculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public TextView david;
+    public Button ingresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        david = (TextView) findViewById(R.id.david);
+        ingresar = (Button) findViewById(R.id.ingresar);
+        ingresar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ingresar:
+                Intent intent = new Intent(this,Calculator.class);
+                startActivity(intent);
+            break;
+        }
     }
 }
