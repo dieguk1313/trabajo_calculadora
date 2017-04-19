3028987e06a2553ec12147b2bb5232dd55af4720
David Montoya <jdmontoyam@gmail.com>
2017-04-18 20:12:23 -0500

    Se crea la logica del proyecto y funciona bien

--- app/src/main/java/com/app/playtolife/calculator/Calculator.java
+++ app/src/main/java/com/app/playtolife/calculator/Calculator.java
@@ -1,13 +1,68 @@
 package com.app.playtolife.calculator;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Calculator extends AppCompatActivity {
+
+    RadioGroup radioGroup;
+    TextView resultado;
+    EditText numberOne;
+    EditText numberTwo;
+
+    float result,valorUno,valorTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        resultado = (TextView) findViewById(R.id.textView4);
+
+        numberOne = (EditText) findViewById(R.id.editText7);
+        numberTwo = (EditText) findViewById(R.id.editText8);
+
+        radioGroup = (RadioGroup)findViewById(R.id.radiogrupo);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+                radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+                        if(i == R.id.suma){
+                            valorUno = Integer.parseInt(numberOne.getText().toString());
+                            valorTwo = Integer.parseInt(numberTwo.getText().toString());
+                            result = valorUno+valorTwo;
+
+                            resultado.setText(String.valueOf(result));
+                        }else if(i == R.id.resta){
+                            valorUno = Integer.parseInt(numberOne.getText().toString());
+                            valorTwo = Integer.parseInt(numberTwo.getText().toString());
+                            result = valorUno-valorTwo;
+
+                            resultado.setText(String.valueOf(result));
+                        }else if(i == R.id.multiplicacion){
+                            valorUno = Integer.parseInt(numberOne.getText().toString());
+                            valorTwo = Integer.parseInt(numberTwo.getText().toString());
+                            result = valorUno*valorTwo;
+
+                            resultado.setText(String.valueOf(result));
+                        }else if (i == R.id.division){
+                            valorUno = Integer.parseInt(numberOne.getText().toString());
+                            valorTwo = Integer.parseInt(numberTwo.getText().toString());
+                            result = valorUno/valorTwo;
+
+                            resultado.setText(String.valueOf(result));
+                        }
+                    }
+                });
+            }
+        });
     }
 }
